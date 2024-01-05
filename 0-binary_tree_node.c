0e1906a64090721e9d76998c4b0107828f93ec59
root <zeusgolohor@gmail.com>
2024-01-04 16:45:32 -0800

    second commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - A function to create a new binary
+ * tree node.
+ * @parent: the new node parent node.
+ * @value: data to be inserted into the new node.
+ * Reurn: binary_tree_t *.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+/**
+	binary_tree_t *newnode = NULL;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode != NULL)
+		return (NULL);
+	return (NULL);
+	newnode->parent = malloc(sizeof(binary_tree_t));
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);*/
+}
